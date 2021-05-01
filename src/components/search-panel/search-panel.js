dfac5fa17d448ad1d2a4d192111e7a3e75e659c2
YU-K <xver456@gmail.com>
2021-05-01 23:02:34 +0400

    refactor search-panel

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./search-panel.css";
+
+const SearchPanel = () => (
+  <input
+    type="text"
+    className="form-control search-input"
+    placeholder="search"
+  />
+);
+
+export default SearchPanel;
