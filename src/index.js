32739bf8b944f443a707fc44fd1b166a0c755cc7
YU-K <xver456@gmail.com>
2021-05-01 12:56:44 +0400

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+import React from "react";
+import ReactDom from "react-dom";
+
+import AppHeader from "./components/AppHeader";
+import SearchPanel from "./components/SearchPanel";
+import TodoList from "./components/TodoList";
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
+};
+
+ReactDom.render(<App />, document.getElementById("root"));
