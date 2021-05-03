9ed91bc066b987d5134995bd1b5a5423e524ab85
YU-K <xver456@gmail.com>
2021-05-03 12:29:49 +0400

    add onToggleImportant, onToggleDone

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -4,12 +4,18 @@ import "./todo-list.css";
 
 import TodoListItem from "../todo-list-item";
 
-const TodoList = ({ todos, onDeleted }) => {
+const TodoList = ({ todos, onDeleted, onToggleDone, onToggleImportant }) => {
   const elements = todos.map((todo) => {
     const { id, ...rest } = todo;
     return (
       <li key={id} className="list-group-item">
-        <TodoListItem {...rest} onDeleted={() => onDeleted(id)} />;
+        <TodoListItem
+          {...rest}
+          onToggleImportant={() => onToggleImportant(id)}
+          onToggleDone={() => onToggleDone(id)}
+          onDeleted={() => onDeleted(id)}
+        />
+        ;
       </li>
     );
   });
